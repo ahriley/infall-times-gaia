ecf11e53d1ff4ad8c9d926409e597990f43de64b
ahriley <30327239+ahriley@users.noreply.github.com>
2018-06-06 14:22:57 -0700

    exploring R_0 issue in Rocha potential integration

--- NFW_potentials.py
+++ NFW_potentials.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 from utils import *
+from scipy.integrate import quad
 
 G = 1.327*10**11    # km^3 / (solar mass * s^2)
+G2 = G*km2kpc**3
 
 def potential(subhalos, hosts):
     r = subhalos.r*kpc2km
@@ -10,8 +12,16 @@ def potential(subhalos, hosts):
     rho0 = hosts.loc[subs['hostID']].rho0.values/(kpc2km**3)
     return -4*np.pi*G*rho0*Rs**3*np.log(1+r/Rs)/r
 
-for sim in list_of_sims('elvis'):
-    halos = load_elvis(sim).iloc[0:2]
+def mltr(r, rho0, rs):
+    return 4*np.pi*rho0*rs**3*(np.log((rs+r)/rs) - r/(rs+r))
+
+def potential2(r, rho0, rs, rmax):
+    func = lambda r: G2*mltr(r, rho0, rs)/r**2
+    return -quad(func, r, rmax)[0]
+
+for sim in ['iHall_HiRes', 'iScylla_HiRes', 'iKauket_HiRes']:
+    nhosts = 2 if '&' in sim else 1
+    halos = load_elvis(sim).iloc[0:nhosts]
     subs = pd.read_pickle('derived_props/'+sim)
 
     # approx potential as NFW, concentrations from ELVIS paper Table 2
@@ -20,6 +30,8 @@ for sim in list_of_sims('elvis'):
     halos['rho0'] = halos.M_dm/(4*np.pi*halos.Rs**3)
     halos['rho0'] /= np.log(1+halos.c) - (halos.c/(1+halos.c))
 
-    # calculate potential, save result
+    # calculate potential two ways, save result
     subs['pot_NFW'] = potential(subs, halos)
+    subs['pot_NFW_1Mpc'] = [potential2(rval,halos.rho0,halos.Rs, 10**3)*kpc2km**2 for rval in subs.r]
+    subs['pot_NFW_inf'] = [potential2(rval,halos.rho0,halos.Rs, np.inf)*kpc2km**2 for rval in subs.r]
     subs.to_pickle('derived_props/'+sim)
