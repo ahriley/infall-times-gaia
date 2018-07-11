da9660ac6b8ec711e548dfc4e6f11316a42f1a9c
ahriley <30327239+ahriley@users.noreply.github.com>
2018-07-11 17:56:10 -0600

    updated plots, slopes

--- plots.py
+++ plots.py
@@ -4,25 +4,27 @@ import pandas as pd
 from utils import *
 from astropy.cosmology import WMAP7
 
-# """
 # Rocha plots for ELVIS
 r = np.array([])
 for sim in list_of_sims('elvis'):
-    subs = pd.read_pickle('derived_props/'+sim)
+    halos, subs = load_elvis(sim=sim, processed=True)
     r = np.append(r, subs.r)
 
 max_r = np.max(r)
 
 bind, z, r, v_r = [np.array([]) for i in range(4)]
 for sim in list_of_sims('elvis'):
-    if '&' in sim:
+    if sim[0] != 'i' or 'HiRes' in sim:
         continue
-    elif 'HiRes' in sim:
+    try:
+        halos, subs = load_elvis(sim=sim, processed=True)
+        subs = subs[subs.nadler2018 > 0.5]
+        pot = subs.pot_mltr
+    except AttributeError:
+        print(sim+" not included")
         continue
-    elif sim in ['iOates', 'iKauket', 'iHera', 'iZeus', 'iScylla', 'iSerena']:
-        continue
-    subs = pd.read_pickle('derived_props/'+sim)
-    bind_sim = -subs.pot_NFW2.values - 0.5*(subs.v_r.values**2 + subs.v_t.values**2)
+
+    bind_sim = -pot - 0.5*(subs.v_r.values**2 + subs.v_t.values**2)
     bind = np.append(bind, bind_sim)
     z = np.append(z, WMAP7.lookback_time(1/subs.a_acc.values - 1))
     r = np.append(r, subs.r)
@@ -46,29 +48,30 @@ for sim in list_of_sims('elvis'):
     plt.close()
     """
 plt.scatter(z[(bind>0)], np.log10(bind[bind>0]), c=r[bind>0], s=2., cmap='plasma')
-plt.ylim(3.4,5.2)
+plt.ylim(2.5,5.2)
 plt.colorbar().set_label(r'Galactocentric Radius [$kpc$]')
 plt.xlabel(r'Infall time [$Gyr$]')
 plt.ylabel(r'log(Binding Energy) [$km^2\ s^{-2}$]');
-plt.savefig('figures/isolated_behaved.png', bbox_inches='tight')
-# """
+plt.savefig('figures/isolated.png', bbox_inches='tight')
 
 """
 # plot for single halo
-subs = pd.read_pickle('derived_props/iScylla_HiRes')
+halos, subs = load_vl2(scale=1.0, processed=True)
+# subs = subs[subs.nadler2018 > 0.5]
 z = WMAP7.lookback_time(1/subs.a_acc.values - 1)
+# z = subs.a_acc.values
 r = subs.r.values
 v_r = subs.v_r
-bind = -subs.pot_NFW.values - 0.5*(subs.v_r.values**2 + subs.v_t.values**2)
+bind = -subs.pot_mltr_1000.values - 0.5*(subs.v_r.values**2 + subs.v_t.values**2)
 
-plt.scatter(z[bind>0], np.log10(bind[bind>0]), s=2.0, c=r[bind>0], cmap='plasma')
+plt.scatter(z[bind>0], np.log10(bind[bind>0]), s=10.0, c=r[bind>0], cmap='plasma')
 plt.colorbar().set_label(r'Galactocentric Radius [$kpc$]')
-plt.title('iScylla_HiRes')
+plt.title('VL2')
 plt.ylim(3.4,5.2)
 plt.yticks([3.5,4.0,4.5,5.0])
 plt.xlabel(r'Infall time [$Gyr$]')
 plt.ylabel(r'log(Binding Energy) [$km^2\ s^{-2}$]');
-plt.savefig('figures/rocha_fig1_iScylla_HiRes.png', bbox_inches='tight')
+# plt.savefig('figures/rocha_fig1_iScylla_HiRes.png', bbox_inches='tight')
 plt.close()
 
 plt.scatter(r[bind>0], v_r[bind>0], s=2.0, c=z[bind>0], cmap='plasma')
@@ -76,6 +79,6 @@ plt.colorbar().set_label(r'Infall time [$Gyr$]')
 plt.xlabel(r'Galactocentric Radius [$kpc$]')
 plt.ylabel(r'Radial Velocity [$km/s$]');
 plt.title('iScylla_HiRes')
-plt.savefig('figures/rocha_fig3_iScylla_HiRes.png', bbox_inches='tight')
+# plt.savefig('figures/rocha_fig3_iScylla_HiRes.png', bbox_inches='tight')
 plt.close()
-"""
+# """
--- slopes.py
+++ slopes.py
@@ -12,11 +12,18 @@ simlist = []
 for sim in simlist_full:
     if sim[0] == 'i' and 'HiRes' not in sim:
         simlist.append(sim)
+simlist.append('vl2')
 
-slopes, Mvir, c, apeak, error, mass_change, Mpeak = [], [], [], [], [], [], []
+slopes, b, Mvir, c, apeak, error, mass_change, Mpeak, Vmax = [[] for i in range(9)]
 for sim in simlist:
-    subs = pd.read_pickle('derived_props/'+sim)
-    bind = -subs.pot_NFW2 - 0.5*(subs.v_r**2 + subs.v_t**2)
+    if sim == 'vl2':
+        halos, subs = load_vl2(scale=1.0, processed=True)
+        subs = subs[subs.Vmax > 8]
+    else:
+        halos, subs = load_elvis(sim=sim, processed=True)
+    # subs = subs[subs.nadler2018 > 0.5]
+    pot = subs.pot_mltr
+    bind = -pot - 0.5*(subs.v_r**2 + subs.v_t**2)
     infall = WMAP7.lookback_time(1/subs.a_acc - 1).value
     infallcut = infall > 2
     bindcut = bind > 0
@@ -24,31 +31,25 @@ for sim in simlist:
     infall_ = infall[infallcut & bindcut]
 
     # linear regression
-    slope, intercept, r_value, p_value, std_err = linregress(infall_, np.log10(bind_))
-    pred = intercept + slope*infall[bindcut]
-    """
-    plt.scatter(WMAP7.lookback_time(1/subs.a_acc.values - 1)[bindcut],
-                np.log10(bind[bindcut]), s=2.0, c=subs.r[bindcut],
-                cmap='plasma', vmin=0.0, vmax=370)
-    plt.plot(WMAP7.lookback_time(1/subs.a_acc.values - 1).value, intercept + slope*WMAP7.lookback_time(1/subs.a_acc.values - 1).value, 'r')
-    plt.colorbar().set_label(r'Galactocentric Radius [$kpc$]')
-    plt.xlim(0.0, WMAP7.lookback_time(np.inf).value)
-    plt.ylim(3.4,5.2)
-    plt.yticks([3.5,4.0,4.5,5.0])
-    plt.xlabel(r'Infall time [$Gyr$]')
-    plt.ylabel(r'log(Binding Energy) [$km^2\ s^{-2}$]');
-    plt.savefig('figures/eachvolume/slope_'+sim+'.png', bbox_inches='tight')
-    plt.close()
-    """
+    slope, intercept, r_value = linregress(infall_, np.log10(bind_))[:3]
+    pred = intercept + slope*infall_
+
     # save the params
-    halo = load_elvis(sim).iloc[0]
-    mass_change.append((halo.Mpeak - halo.M_dm)/halo.M_dm)
     slopes.append(slope)
-    Mvir.append(halo.M_dm)
+    error.append(MSE(np.log10(bind_),pred))
+    b.append(intercept)
+    if sim == 'vl2':
+        Mvir.append(1.93596e+12)
+        Vmax.append(201.033)
+        c.append(halo_concentrations('vl2')[0])
+        continue
+    halo = halos.iloc[0]
+    mass_change.append((halo.Mpeak - halo.Mvir)/halo.Mvir)
+    Mvir.append(halo.Mvir)
     c.append(halo_concentrations(sim)[0])
     apeak.append(halo.apeak)
     Mpeak.append(halo.Mpeak)
-    error.append(MSE(np.log10(bind[bindcut]),pred))
+    Vmax.append(halo.Vmax)
 slopes = np.array(slopes)
 Mvir = np.array(Mvir)
 c = np.array(c)
@@ -56,24 +57,14 @@ error = np.array(error)
 mass_change = np.array(mass_change)
 apeak = np.array(apeak)
 
-plt.scatter(c, slopes, c=np.log10(Mvir), cmap='plasma')
-plt.colorbar().set_label(r'log(Mvir) [$M_\odot$]')
-plt.xlabel('c')
-plt.ylabel('Slope');
-plt.savefig('figures/slope_c_mvir.png', bbox_inches='tight')
-plt.close()
-
-plt.scatter(c, slopes, c=1/apeak-1, cmap='plasma_r')
-plt.plot(12.2, 0.10688449504116676, 'ks')
-plt.colorbar().set_label(r'z_peak')
-plt.xlabel('c')
-plt.ylabel('Slope');
-plt.savefig('figures/slope_c_zpeak.png', bbox_inches='tight')
-plt.close()
-
-plt.scatter(1/apeak[mass_change>0]-1, mass_change[mass_change>0], c=Mvir[mass_change>0], cmap='plasma')
-plt.xlabel('z_peak')
-plt.ylabel('(Mpeak-Mvir(z=0)) / Mpeak');
-plt.colorbar().set_label(r'Mvir')
-plt.savefig('figures/massloss_zpeak.png', bbox_inches='tight')
-plt.close()
+x = np.log10(Mvir)
+color = c
+y = slopes
+k = {'cmap': 'plasma', 'vmin': np.min(color), 'vmax': np.max(color)}
+plt.scatter(x[:-1], y[:-1], c=color[:-1], **k)
+plt.scatter(x[-1], y[-1], c=color[-1], marker='*', s=200, **k)
+plt.colorbar().set_label(r'Concentration');
+plt.title(r'MSE vs. Halo Params ($\Phi_{M(<r)}(500) = 0$)')
+plt.xlabel(r'$\log(M_{vir}) [M_\odot]$')
+plt.ylabel(r'$d\log(E_{bind})/dt_{infall}$');
+plt.savefig('figures/spread_mvir_c.png', bbox_inches='tight')
