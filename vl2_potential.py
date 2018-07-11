adb2409e9229f5bb08a881579927ef05f55d0a3f
ahriley <30327239+ahriley@users.noreply.github.com>
2018-07-11 16:02:42 -0600

    interpolating between snapshots for VL2

--- vl2_potential.py
+++ vl2_potential.py
@@ -5,10 +5,10 @@ from scipy.interpolate import interp1d
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
 
-G = 1.327*10**11*km2kpc**3    # kpc^3 / (solar mass * s^2)
+G = 1.327*10**11*(1/kpc2km)**3    # kpc^3 / (solar mass * s^2)
 
 # load z=0 halos
-halo = load_vl2(scale=1.0).iloc[0]
+halos, subs = load_vl2(scale=1.0, processed=True)
 
 # enclosed mass by particles out to Rvir(z=0)
 parts = pd.read_table(VL2_DIR+'vl2subset_relposvel.txt', sep=' ')
@@ -19,10 +19,23 @@ mltr = np.array([np.sum(parts.r < rval) for rval in rvals]) * 2.823*10**7
 # interpolating function for M(<r)
 mltr_interp = interp1d(rvals, mltr, kind='cubic')
 
-def potential(r):
+def potential(r, R0):
     func = lambda r: G*mltr_interp(r)/r**2
-    return -quad(func, r, 10**3)[0]
+    return -quad(func, r, R0)[0]
 
-subs = pd.read_pickle('derived_props/vl2')
-subs['pot_grav'] = [potential(r)*kpc2km**2 for r in subs.r]
-subs.to_pickle('derived_props/vl2')
+props = pd.read_pickle('derived_props/vl2')
+
+# approx potential as NFW, z=0 concentration from Emberson+ 2015
+halos['c'] = halo_concentrations('vl2')[0]
+halos['Rs'] = halos['Rvir']/halos['c']
+halos['rho0'] = halos.Mvir/(4*np.pi*halos.Rs**3)
+halos['rho0'] /= np.log(1+halos.c) - (halos.c/(1+halos.c))
+
+# calculate potential multiple ways, save result
+R0 = radii_shea()[-1]
+props['pot_NFW'] = potentialNFW(subs, halos)
+props['pot_NFW_1000'] = potentialNFW_R0(subs, halos, R0=1000)
+props['pot_NFW_approx_mltr'] = potentialNFW_R0(subs, halos, R0=R0)
+props['pot_mltr_1000'] = [potential(r, R0=10**3)*kpc2km**2 for r in subs.r]
+props['pot_mltr'] = [potential(r, R0=R0)*kpc2km**2 for r in subs.r]
+props.to_pickle('derived_props/vl2')
